60cbe67fbfaad3a28b600ff0d4b3f1a27d939d4a
CodeMyst <code@myst.rs>
2022-09-02 21:56:00 +0200

    drawing the map

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <tuple>
 #include <fstream>
 #include <cassert>
+#include <algorithm>
 #include "raylib.h"
 
 enum Tile {
@@ -45,11 +46,28 @@ std::tuple<int, int, std::vector<Tile>> loadMap(const std::string& path) {
     return {width, height, map};
 }
 
+void drawMap(int width, int height, std::vector<Tile> map) {
+    DrawRectangle(0, 0, (width + 1) * 10, (height + 1) * 10, (Color){28, 28, 28, 255});
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            Tile tile = map.at(x + y * width);
+
+            if (tile == Tile::Wall) {
+                DrawRectangle(x * 10, y * 10, 10, 10, (Color){204, 204, 204, 255});
+            }
+        }
+    }
+}
+
 int main() {
     auto [width, height, map] = loadMap("maps/map01.txt");
 
-    std::cout << width << ", " << height << std::endl;
-    std::cout << map.size() << std::endl;
+    auto it = std::find(map.begin(), map.end(), Tile::Player);
+    assert(it != map.end());
+
+    int playerX = (int) (it - map.begin()) % width;
+    int playerY = (int) (it - map.begin()) / width;
 
     InitWindow(1280, 720, "tinyraycaster");
 
@@ -57,6 +75,10 @@ int main() {
         BeginDrawing();
 
         ClearBackground((Color){11, 11, 11, 255});
+
+        drawMap(width, height, map);
+
+        DrawRectangle(playerX * 10 + 2, playerY * 10 + 2, 5, 5, BLUE);
 
         EndDrawing();
     }
