07b8c52b3364c8e1ca69c2f4eee2b9ca60e7de21
CodeMyst <code@myst.rs>
2022-09-08 13:44:04 +0200

    multiple colored wall types

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,30 @@
 #include <cmath>
 #include "raylib.h"
 
-enum Tile {
+enum TileType {
     Empty,
     Wall,
     Player
 };
+
+struct Tile {
+    TileType type;
+    uint8_t texIdx = 0;
+};
+
+Color texIdxToCol(int idx) {
+    if (idx == 1) {
+        return (Color){208, 208, 208, 255};
+    } else if (idx == 2) {
+        return (Color){209, 27, 42, 255};
+    } else if (idx == 3) {
+        return (Color){171, 72, 29, 255};
+    } else if (idx == 5) {
+        return (Color){41, 99, 20, 255};
+    }
+
+    return (Color){0, 0, 0, 255};
+}
 
 std::tuple<int, int, std::vector<Tile>> loadMap(const std::string& path) {
     int width = 0;
@@ -28,11 +47,21 @@ std::tuple<int, int, std::vector<Tile>> loadMap(const std::string& path) {
         Tile t;
 
         if (c == '-') {
-            t = Tile::Empty;
-        } else if (c == '#') {
-            t = Tile::Wall;
+            t.type = TileType::Empty;
         } else if (c == '@') {
-            t = Tile::Player;
+            t.type = TileType::Player;
+        } else {
+            t.type = TileType::Wall;
+
+            if (c == '#') {
+                t.texIdx = 1;
+            } else if (c == 'X') {
+                t.texIdx = 2;
+            } else if (c == 'V') {
+                t.texIdx = 5;
+            } else if (c == '|') {
+                t.texIdx = 3;
+            }
         }
 
         if (c == '\n') {
@@ -54,8 +83,8 @@ void drawMap(int width, int height, std::vector<Tile> map) {
         for (int x = 0; x < width; x++) {
             Tile tile = map.at(x + y * width);
 
-            if (tile == Tile::Wall) {
-                DrawRectangle(x * 10, y * 10, 10, 10, (Color){204, 204, 204, 255});
+            if (tile.type == TileType::Wall) {
+                DrawRectangle(x * 10, y * 10, 10, 10, texIdxToCol(tile.texIdx));
             }
         }
     }
@@ -64,7 +93,9 @@ void drawMap(int width, int height, std::vector<Tile> map) {
 int main() {
     auto [width, height, map] = loadMap("maps/map01.txt");
 
-    auto it = std::find(map.begin(), map.end(), Tile::Player);
+    auto it = std::find_if(map.begin(), map.end(), [](const Tile tile) {
+        return tile.type == TileType::Player;
+    });
     assert(it != map.end());
 
     float playerX = (float) ((int) (it - map.begin()) % width);
@@ -74,22 +105,24 @@ int main() {
     float fov = M_PI / 3.0;
 
     InitWindow(1280, 720, "tinyraycaster");
+    SetTargetFPS(75);
     SetTraceLogLevel(LOG_NONE);
+
+    Texture2D tex = LoadTexture("assets/walltext.png");
 
     while (!WindowShouldClose()) {
         BeginDrawing();
 
         ClearBackground((Color){11, 11, 11, 255});
 
-        SetTargetFPS(75);
         DrawFPS(1280 - 100, 0);
+
+        const int numRays = 320;
 
         drawMap(width, height, map);
 
         // draw player on map
         DrawRectangle(playerX * 10 - 2, playerY * 10 - 2, 5, 5, BLUE);
-
-        const int numRays = 320;
 
         // draw rays on map
         for (size_t i = 0; i < numRays; i++) {
@@ -107,11 +140,18 @@ int main() {
                 if (cx < 0 || cx > width ||
                     cy < 0 || cy > height ||
                     idx > map.size() - 1 ||
-                    map.at(idx) == Tile::Wall) {
+                    map.at(idx).type == TileType::Wall) {
 
                     double colHeight = 720.0 / (c * std::cos(angle - playerAngle));
                     double y = 720.0 / 2.0 - colHeight / 2.0;
-                    DrawRectangle(i * 4, y, 4, colHeight, (Color){208, 208, 208, 255});
+
+                    Color col = (Color){0, 0, 0, 255};
+                    if (idx < map.size()) {
+                        int texIdx = map.at(idx).texIdx;
+                        col = texIdxToCol(texIdx);
+                    }
+
+                    DrawRectangle(i * 4, y, 4, colHeight, col);
                     break;
                 }
 
@@ -137,6 +177,8 @@ int main() {
 
         EndDrawing();
     }
+
+    UnloadTexture(tex);
 
     CloseWindow();
 
