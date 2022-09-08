3b9cbe60e47c39899e134f93ad6642198f157ea6
CodeMyst <code@myst.rs>
2022-09-08 19:26:54 +0200

    drawing sprites on map

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,13 @@ enum TileType {
 
 struct Tile {
     TileType type;
-    uint8_t texIdx = 0;
+    uint8_t texId = 0;
+};
+
+struct Sprite {
+    float x;
+    float y;
+    int texId;
 };
 
 Color texIdxToCol(int idx) {
@@ -54,13 +60,13 @@ std::tuple<int, int, std::vector<Tile>> loadMap(const std::string& path) {
             t.type = TileType::Wall;
 
             if (c == '#') {
-                t.texIdx = 1;
+                t.texId = 1;
             } else if (c == 'X') {
-                t.texIdx = 2;
+                t.texId = 2;
             } else if (c == 'V') {
-                t.texIdx = 5;
+                t.texId = 5;
             } else if (c == '|') {
-                t.texIdx = 3;
+                t.texId = 3;
             }
         }
 
@@ -76,17 +82,19 @@ std::tuple<int, int, std::vector<Tile>> loadMap(const std::string& path) {
     return {width, height, map};
 }
 
-void drawMap(int width, int height, std::vector<Tile> map) {
-    DrawRectangle(0, 0, (width + 1) * 10, (height + 1) * 10, (Color){28, 28, 28, 255});
-
+void drawMap(int width, int height, std::vector<Tile> map, std::vector<Sprite> sprites) {
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             Tile tile = map.at(x + y * width);
 
             if (tile.type == TileType::Wall) {
-                DrawRectangle(x * 10, y * 10, 10, 10, texIdxToCol(tile.texIdx));
+                DrawRectangle(x * 10, y * 10, 10, 10, texIdxToCol(tile.texId));
             }
         }
+    }
+
+    for (Sprite s : sprites) {
+        DrawRectangle(s.x * 10 - 2, s.y * 10 - 2, 5, 5, (Color){204, 20, 20, 255});
     }
 }
 
@@ -104,6 +112,8 @@ int main() {
     float playerAngle = 1.5;
     float fov = M_PI / 3.0;
 
+    std::vector<Sprite> sprites{{1.834, 8.765, 0}, {5.323, 1.365, 1}, {4.123, 10.265, 1}};
+
     InitWindow(1280, 720, "tinyraycaster");
     SetTargetFPS(75);
     SetTraceLogLevel(LOG_NONE);
@@ -118,11 +128,6 @@ int main() {
         DrawFPS(1280 - 100, 0);
 
         const int numRays = 320;
-
-        drawMap(width, height, map);
-
-        // draw player on map
-        DrawRectangle(playerX * 10 - 2, playerY * 10 - 2, 5, 5, BLUE);
 
         // draw rays on map
         for (size_t i = 0; i < numRays; i++) {
@@ -142,12 +147,12 @@ int main() {
                     idx > map.size() - 1 ||
                     map.at(idx).type == TileType::Wall) {
 
-                    double colHeight = 720.0 / (c * std::cos(angle - playerAngle));
-                    double y = 720.0 / 2.0 - colHeight / 2.0;
+                    float colHeight = 720.0 / (c * std::cos(angle - playerAngle));
+                    float y = 720.0 / 2.0 - colHeight / 2.0;
 
                     Color col = (Color){0, 0, 0, 255};
                     if (idx < map.size()) {
-                        int texIdx = map.at(idx).texIdx;
+                        int texIdx = map.at(idx).texId;
                         col = texIdxToCol(texIdx);
                     }
 
@@ -167,16 +172,22 @@ int main() {
 
                     assert(texCoord >= 0 && texCoord < 64);
 
-                    DrawTexturePro(tex, (Rectangle){(64 * map.at(idx).texIdx) + texCoord, 0, 1, 64}, (Rectangle){i * 4, y, 4, colHeight}, (Vector2){0, 0}, 0, WHITE);
+                    DrawTexturePro(tex, (Rectangle){(float) ((64 * map.at(idx).texId) + texCoord), 0, 1, 64},
+                        (Rectangle){(float) (i * 4), y, 4, colHeight}, (Vector2){0, 0}, 0, WHITE);
 
                     break;
                 }
 
-                DrawPixel(cx * 10, cy * 10, RED);
+                DrawPixel(cx * 10, cy * 10, (Color){130, 130, 130, 255});
 
                 c += 0.01;
             }
         }
+
+        drawMap(width, height, map, sprites);
+
+        // draw player on map
+        DrawRectangle(playerX * 10 - 2, playerY * 10 - 2, 5, 5, BLUE);
 
         const float movSpeed = 0.05;
         const float lookSpeed = 1.5;
